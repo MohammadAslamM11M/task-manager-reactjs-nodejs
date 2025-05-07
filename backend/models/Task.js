6913de535dd97e519df7ebd50b6fa08877ecbbef
Mohammad Aslam <aslam071595@gmail.com>
2025-05-07 21:06:18 +0530

    user and task model schema created

---
+++ backend/models/Task.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const taskSchema = new mongoose.Schema({
+  title: String,
+  description: String,
+  status: {
+    type: String,
+    default: "incomplete",
+  },
+  priority: {
+    type: String,
+    enum: ["Low", "Medium", "High"],
+    default: "Low",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+});
+
+module.exports = mongoose.model("Task", taskSchema);
