f561d3ac32f019d1b6f0730fca051b9f66cdf127
Mohammad Aslam <aslam071595@gmail.com>
2025-05-08 13:17:03 +0530

    added routes and dotenv to the server.js

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,25 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const authRoutes = require("./routes/authRoutes");
+const taskRoutes = require("./routes/taskRoutes");
 
+dotenv.config();
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.listen(5000, () => console.log(`Server running on port 5000`));
+app.use("/api/auth", authRoutes);
+app.use("/api/tasks", taskRoutes);
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(5000, () => console.log(`Server running on port 5000`))
+  )
+  .catch((err) => console.log(err));
