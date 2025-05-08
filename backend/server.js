a3652606f5239e55fa5dcd90b13eaee8e7a43091
Mohammad Aslam <aslam071595@gmail.com>
2025-05-08 20:19:07 +0530

    server file updated for deprecated keywords

--- backend/server.js
+++ backend/server.js
@@ -20,10 +20,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/tasks", taskRoutes);
 
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URI)
   .then(() =>
     app.listen(5000, () => console.log(`Server running on port 5000`))
   )
